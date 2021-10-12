d8d6d737466e846d19c64aab7259eb79bf852aee
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2021-10-12 14:06:27 -0400

    created two forms that take in user input to add and delete nodes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ function App() {
   return (
     <div className="App">
       <h1>Linked List Visualizer</h1>
-      <form
+      <form>
+        <label>
+          Value:
+          <input type="text" name="value" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <form>
+        <label>
+          id:
+          <input type="text" name="id" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
